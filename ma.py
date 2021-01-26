cbb53aa9e7dae4341abb717c7039982c7f9c6baa
MiniPanda03 <pandabogusawska@gmail.com>
2021-01-26 22:03:05 +0100

    Delete ma.py

--- ma.py
+++
@@ -1,8 +0,0 @@
-def miesiac():
-    miesiac= ["styczen","luty","marzec","kwiecien","maj","czerwiec","lipiec","sierpien","wrzesien","pazdziernik","listopad","grudzien"]
-    i = int(input("Podaj liczbe"))
-    if i>12 or i<=0:
-        print("Podaj poprawną liczbę")
-    else:
-        print(miesiac[i-1])
-miesiac()
